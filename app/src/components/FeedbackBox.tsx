14ead0af8b73c79912415ba86dd9511bd718ec95
Mark Gray <mfgray87@gmail.com>
2023-09-03 22:13:55 +0100

    fixed bug where comment wasn't being sent to the API

--- app/src/components/FeedbackBox.tsx
+++ app/src/components/FeedbackBox.tsx
@@ -5,17 +5,19 @@ interface FeedbackBoxProps {
     setComment: Dispatch<SetStateAction<string>>;
 }
 
-const FeedbackBox: React.FC<FeedbackBoxProps> = () => {
-    const [comment, setComment] = useState('');
-
-  
+const FeedbackBox: React.FC<FeedbackBoxProps> = ({setComment}) => {
+    const [feedback, setFeedback] = useState('');
 
   return (
     <IonItem>
     <IonLabel position="floating">Enter your comment about the place (up to 160 characters)</IonLabel>
     <IonTextarea 
-        value={comment}
-        onIonChange={(e: any) => setComment(e.detail.value)}
+        value={feedback}
+        onIonChange={(e: any) => {
+            const newValue = e.detail.value;
+            setFeedback(newValue);
+            setComment(newValue);
+        }}
         maxlength={160}
         rows={4}
         placeholder="Your Comment"
